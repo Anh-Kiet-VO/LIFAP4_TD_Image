1606ff4876c16e5faedb6924abdb08c772cd6d84
Anh-Kiet <anhkietakpro@gmail.com>
2021-02-01 22:12:19 +0100

    Rajout des fonctions Sauver / Ouvrir / afficherConsole

--- Image.h
+++ Image.h
@@ -2,6 +2,7 @@
 #define IMAGE_H
 
 #include "Pixel.h"
+#include <string>
 
 class Image{
   private:
@@ -11,16 +12,19 @@ class Image{
   public:
     Image();
     Image(unsigned int dimensionX, unsigned int dimensionY);
-
     ~Image();
 
     Pixel getPix(unsigned int x, unsigned int y) const;
-
     void setPix(const unsigned int x, const unsigned int y, const Pixel& couleur);
 
     void dessinerRectangle(const unsigned int Xmin, const unsigned int Ymin, const unsigned int Xmax, const unsigned int Ymax, const Pixel& couleur);
     void effacer(const Pixel& couleur);
+
     void testRegression();
+
+    void sauver(const string & filename) const;
+    void ouvrir(const string & filename);
+    void afficherConsole();
 };
 
 #endif
