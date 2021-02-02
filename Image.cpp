e2a59ea3f3962ac871cda7a914cc8718f031bc65
Kevin69100 <keazcu@gmail.com>
2021-02-02 11:01:16 +0100

    correction erreur

--- Image.cpp
+++ Image.cpp
@@ -29,9 +29,7 @@ Pixel Image::getPix(unsigned int x, unsigned int y) const {
 
 void Image::setPix(const unsigned int x, const unsigned int y, const Pixel& couleur) {
 	assert(x < dimx && y < dimx);
-	tab[y * dimx + x] = couleur)leur);
-		}
-	}
+	tab[y * dimx + x] = couleur;
 }
 
 void Image::effacer(const Pixel& couleur) {
