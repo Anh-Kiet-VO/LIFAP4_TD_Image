759d414363a42c16576b5f405c22b258f572259c
Anh-Kiet <anhkietakpro@gmail.com>
2021-02-02 13:01:32 +0100

    Résolution problème indentation (Kévin le fautif ?)

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 using namespace std;
 
 int main() {
-        Pixel monPixel (7, 10, 3);
-        Image monImage (500, 500);
-        //cout << monPixel.getRouge() << " " << monPixel.getVert() << " " << monPixel.getBleu();
-        //cout << endl;
-        
-        monImage.dessinerRectangle(0,4,0,8,monPixel);
-        return 0;
+    Pixel monPixel (7, 10, 3);
+    Image monImage (500, 500);
+    //cout << monPixel.getRouge() << " " << monPixel.getVert() << " " << monPixel.getBleu();
+    //cout << endl;
+     
+    monImage.dessinerRectangle(0,4,0,8,monPixel);
+    
+    return 0;
 }
