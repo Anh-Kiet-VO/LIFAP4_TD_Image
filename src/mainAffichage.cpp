7fc3e6180c73f4e74641acf90223560bd39969d7
Anh-Kiet <anhkietakpro@gmail.com>
2021-02-02 19:23:57 +0100

    Création mainAffichage pour SDL2

--- src/mainAffichage.cpp
+++ src/mainAffichage.cpp
@@ -1,3 +1,27 @@
+#include "Image.h"
+ 
+int main (int argc, char** argv) {
+ 
+   Image image (10,10);
+ 
+   Pixel bleu (21, 104, 219);
+   Pixel orange (242, 174, 38);
+   Pixel gris (229, 229, 229);
+ 
+   image.effacer(bleu);
+   image.dessinerRectangle(1, 4, 8, 5, orange);
+   image.dessinerRectangle(2, 3, 6, 6, orange);
+   image.dessinerRectangle(3, 2, 5, 7, orange);
+   image.setPix(8,3,orange);
+   image.setPix(8,6,orange);
+   image.setPix(2,4,gris);
+ 
+   image.afficher();
+ 
+   return 0;
+}
+
+/* - - - - - - - Je sais pas c'est quoi srx - - - - - - - 
 #include "Pixel.h"
 #include "Image.h"
 #include <iostream>
@@ -14,3 +38,4 @@ int main() {
     
     return 0;
 }
+*/
