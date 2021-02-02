94dac62211c87e7c75058371365eadb8dfef6805
Anh-Kiet <anhkietakpro@gmail.com>
2021-02-02 13:04:30 +0100

    Commentaire Doxygene sur le Pixel.h

--- Pixel.h
+++ Pixel.h
@@ -7,50 +7,57 @@ class Pixel
 {
   private:
     /**
-		@brief:
-		 Les composantes du pixel, unsigned int en C++
+      @brief
+		    Les composantes du pixel, unsigned int en C++
 		*/
     unsigned int r, g, b;
 
-public:
-    /** @brief
-		 Les composantes du pixel, unsigned int en C++
-	*/
+  public:
+    /**
+      @brief
+		    Les composantes du pixel, unsigned int en C++
+	  */
     Pixel();
 
-    /** @brief
-		 Constructeur de la classe: initialise r,g,b avec les paramètres
-	*/
+    /**
+      @brief
+		    Constructeur de la classe : initialise r,g,b avec les paramètres
+	  */
     Pixel(unsigned int nr, unsigned int ng, unsigned int nb);
 
-    /** @brief
-		 Accesseur : récupère la composante rouge du pixel
-	*/
+    /**
+      @brief
+		   Accesseur : récupère la composante rouge du pixel
+  	*/
     unsigned int getRouge() const;
 
-    /** @brief
-		 Accesseur : récupère la composante verte du pixel
+    /**
+      @brief
+		    Accesseur : récupère la composante verte du pixel
 		*/
     unsigned int getVert() const;
 
-    /** @brief
-		 Accesseur : récupère la composante bleue du pixel
+    /**
+      @brief
+		    Accesseur : récupère la composante bleue du pixel
 		*/
     unsigned int getBleu() const;
 
-    /** @brief
-		 Mutateur : modifie la composante rouge du pixel
+    /**
+      @brief
+		    Mutateur : modifie la composante rouge du pixel
 		*/
     void setRouge(unsigned int nr);
 
-    /** @brief
-		 Mutateur : modifie la composante verte du pixel
+    /**
+      @brief
+		    Mutateur : modifie la composante verte du pixel
 		*/
     void setVert(unsigned int nv);
 
     /** 
-         @brief
-		Mutateur : modifie la composante bleue du pixel
+      @brief
+		    Mutateur : modifie la composante bleue du pixel
 		*/
     void setBleu(unsigned int nb);
 };
