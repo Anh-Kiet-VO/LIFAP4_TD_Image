e7c70351d12a0859d4c0b4dc940028371e22d81f
Anh-Kiet <anhkietakpro@gmail.com>
2021-02-09 18:08:18 +0100

    mainAffichage

--- src/mainAffichage.cpp
+++ src/mainAffichage.cpp
@@ -20,22 +20,3 @@ int main (int argc, char** argv) {
  
    return 0;
 }
-
-/* - - - - - - - Je sais pas c'est quoi srx - - - - - - - 
-#include "Pixel.h"
-#include "Image.h"
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    Pixel monPixel (7, 10, 3);
-    Image monImage (500, 500);
-    //cout << monPixel.getRouge() << " " << monPixel.getVert() << " " << monPixel.getBleu();
-    //cout << endl;
-     
-    monImage.dessinerRectangle(0,4,0,8,monPixel);
-    
-    return 0;
-}
-*/
