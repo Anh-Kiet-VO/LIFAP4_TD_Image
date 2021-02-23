e3459feee14b90d85dcfd9cdae288ed632a122b5
Kevin69100 <keazcu@gmail.com>
2021-02-23 09:57:08 +0100

    modif pixel.h ajout param commenté

--- src/Pixel.h
+++ src/Pixel.h
@@ -1,71 +1,69 @@
 #ifndef PIXEL_H
 #define PIXEL_H
-
+ 
 class Pixel
 {
   private:
     /**
       @brief
-		    Les composantes du pixel, unsigned int en C++
-		*/
+            Les composantes du pixel, unsigned int en C++
+        */
     unsigned char r, g, b;
-
+ 
   public:
     /**
       @brief
-		    Les composantes du pixel, unsigned int en C++
-	  */
+            Les composantes du pixel, unsigned int en C++
+      */
     Pixel();
-
+ 
     /**
       @brief
-		    Constructeur de la classe : initialise r,g,b avec les paramètres
-      @param
-        nr, ng, nb : entier
-	  */
+            Constructeur de la classe : initialise r,g,b avec les paramètres
+      @param nr : entier
+      @param ng : entier
+      @param nb : entier
+      */
     Pixel(unsigned char nr, unsigned char ng, unsigned char nb);
-
+ 
     /**
       @brief
-		   Accesseur : récupère la composante rouge du pixel
-  	*/
+           Accesseur : récupère la composante rouge du pixel
+    */
     unsigned char getRouge() const;
-
+ 
     /**
       @brief
-		    Accesseur : récupère la composante verte du pixel
-		*/
+            Accesseur : récupère la composante verte du pixel
+        */
     unsigned char getVert() const;
-
+ 
     /**
       @brief
-		    Accesseur : récupère la composante bleue du pixel
-		*/
+            Accesseur : récupère la composante bleue du pixel
+        */
     unsigned char getBleu() const;
-
+ 
     /**
       @brief
-		    Mutateur : modifie la composante rouge du pixel
-      @param
-        nr : entier
-		*/
+            Mutateur : modifie la composante rouge du pixel
+      @param nr : entier
+        */
     void setRouge(unsigned char nr);
-
+ 
     /**
       @brief
-		    Mutateur : modifie la composante verte du pixel
-      @param
-        nv : entier
-		*/
+            Mutateur : modifie la composante verte du pixel
+      @param nv : entier
+        */
     void setVert(unsigned char nv);
-
+ 
     /** 
       @brief
-		    Mutateur : modifie la composante bleue du pixel
-      @param
-        nb : entier
-		*/
+            Mutateur : modifie la composante bleue du pixel
+      @param nb : entier
+        */
     void setBleu(unsigned char nb);
 };
-
+ 
 #endif
